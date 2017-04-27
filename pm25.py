b5743218779e4579c05a70bc1e408ba01b36fe65
ch-tseng <ch.tseng@sunplusit.com>
2017-04-27 09:22:47 +0800

    update

--- pm25.py
+++ pm25.py
@@ -9,6 +9,7 @@ GPIO.setmode(GPIO.BCM)
 
 from struct import *
 from libraryCH.device.lcd import ILI9341
+from libraryCH.device.air import G3
 debug=0
 
 #LCD顯示設定
@@ -43,16 +44,14 @@ def display_data(pm1, pm25, pm10):
     global a_time, a_pm1, a_pm25, a_pm10
 
     dt = list(time.localtime())
-    nowHour = dt[3]
-    nowMinute = dt[4]
-    nowSeconds = dt[5]
 
     lcd.displayClear()
     title = ("%15s %9s %9s %9s" % ("H:M:S", "pm01", "pm2.5", "pm10"))
-    lcd.displayText("e1.ttf", fontSize=16, text=title, position=(lcd_Line2Pixel(1), 40), fontColor=(255,255,255) )
+    lcd.displayText("e1.ttf", fontSize=18, text=title, position=(lcd_Line2Pixel(1), 40), fontColor=(255,255,255) )
+
     for i in range(0,numKeep):
         if(i==(numKeep-1)):
-            a_time[0] = str(nowHour) + ":" + str(nowMinute) + ":" + str(nowSeconds)
+            a_time[0] = str(dt[3]) + ":" + str(dt[4]) + ":" + str(dt[5])
             a_pm1[0] = pm1
             a_pm25[0] = pm25
             a_pm10[0] = pm10
@@ -65,93 +64,21 @@ def display_data(pm1, pm25, pm10):
 
         if(a_time[numKeep-1-i]>0 and a_pm1[numKeep-1-i]>0 and a_pm10[numKeep-1-i]>0):
             displayTXT = ("%13s %11d %11d %11d" % (a_time[numKeep-1-i],a_pm1[numKeep-1-i], a_pm25[numKeep-1-i], a_pm10[numKeep-1-i]))
-            lcd.displayText("e1.ttf", fontSize=16, text=displayTXT, position=(lcd_Line2Pixel(numKeep-i+1), 60), fontColor=(253,244,6) )
+            lcd.displayText("e1.ttf", fontSize=18, text="                                ", position=(lcd_Line2Pixel(numKeep-i+1), 60), fontColor=(253,244,6) )
+            lcd.displayText("e1.ttf", fontSize=18, text=displayTXT, position=(lcd_Line2Pixel(numKeep-i+1), 60), fontColor=(253,244,6) )
 
 #將行數轉為pixels
 def lcd_Line2Pixel(lineNum):
     return lcd_lineHeight*lineNum
 
 
-class g3sensor():
-    def __init__(self):
-        if debug: print "init"
-	self.endian = sys.byteorder
-    
-    def conn_serial_port(self, device):
-        if debug: print device
-        self.serial = serial.Serial(device, baudrate=9600)
-        if debug: print "conn ok"
-
-    def check_keyword(self):
-        if debug: print "check_keyword"
-        while True:
-            token = self.serial.read()
-    	    token_hex=token.encode('hex')
-    	    if debug: print token_hex
-    	    if token_hex == '42':
-    	        if debug: print "get 42"
-    	        token2 = self.serial.read()
-    	        token2_hex=token2.encode('hex')
-    	        if debug: print token2_hex
-    	        if token2_hex == '4d':
-    	            if debug: print "get 4d"
-                    return True
-		elif token2_hex == '00': # fixme
-		    if debug: print "get 00"
-		    token3 = self.serial.read()
-		    token3_hex=token3.encode('hex')
-		    if token3_hex == '4d':
-			if debug: print "get 4d"
-			return True
-		    
-    def vertify_data(self, data):
-	if debug: print data
-        n = 2
-	sum = int('42',16)+int('4d',16)
-        for i in range(0, len(data)-4, n):
-            #print data[i:i+n]
-	    sum=sum+int(data[i:i+n],16)
-	versum = int(data[40]+data[41]+data[42]+data[43],16)
-	if debug: print sum
-        if debug: print versum
-	if sum == versum:
-	    print "data correct"
-	
-    def read_data(self):
-        data = self.serial.read(22)
-        data_hex=data.encode('hex')
-        if debug: self.vertify_data(data_hex)
-        pm1_cf=int(data_hex[4]+data_hex[5]+data_hex[6]+data_hex[7],16)
-        pm25_cf=int(data_hex[8]+data_hex[9]+data_hex[10]+data_hex[11],16)
-        pm10_cf=int(data_hex[12]+data_hex[13]+data_hex[14]+data_hex[15],16)
-        pm1=int(data_hex[16]+data_hex[17]+data_hex[18]+data_hex[19],16)
-        pm25=int(data_hex[20]+data_hex[21]+data_hex[22]+data_hex[23],16)
-        pm10=int(data_hex[24]+data_hex[25]+data_hex[26]+data_hex[27],16)
-        if debug: print "pm1_cf: "+str(pm1_cf)
-        if debug: print "pm25_cf: "+str(pm25_cf)
-        if debug: print "pm10_cf: "+str(pm10_cf)
-        if debug: print "pm1: "+str(pm1)
-        if debug: print "pm25: "+str(pm25)
-        if debug: print "pm10: "+str(pm10)
-        data = [pm1_cf, pm10_cf, pm25_cf, pm1, pm10, pm25]
-    	self.serial.close()
-        return data
-
-    def read(self, argv):
-        tty=argv[0:]
-        self.conn_serial_port(tty)
-        if self.check_keyword() == True:
-            self.data = self.read_data()
-            if debug: print self.data
-            return self.data
-
-air=g3sensor()
+air=G3()
 
 while True:
+    #air=g3sensor()
     pmdata = (air.read("/dev/ttyS0"))
     print (pmdata[3], pmdata[4], pmdata[5])
     display_data(pmdata[3], pmdata[4], pmdata[5])
-    time.sleep(1)
 
     if(pmdata[4]<20):
         GPIO.output(pinGREEN, GPIO.HIGH)
@@ -166,3 +93,4 @@ while True:
         GPIO.output(pinRED, GPIO.HIGH)
         GPIO.output(pinYELLOW, GPIO.LOW)
 
+    time.sleep(6)
